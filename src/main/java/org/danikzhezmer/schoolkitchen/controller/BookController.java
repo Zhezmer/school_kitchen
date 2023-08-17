7fa7014df3ac27ed08e9ddd1851b3c6aa4998531
Danik Zhezmer <danik_zhezmer@mail.ru>
2023-08-17 20:59:49 +0300

    added update methode

--- src/main/java/org/danikzhezmer/schoolkitchen/controller/BookController.java
+++ src/main/java/org/danikzhezmer/schoolkitchen/controller/BookController.java
@@ -19,28 +19,39 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
-    public String getBook(@PathVariable("id") Long id, Model model){
+    public String getBook(@PathVariable("id") Long id, Model model) {
         Book book = bookRepository.findById(id).orElse(null);
         model.addAttribute("book", book);
         return "book_card";
     }
 
     @GetMapping
-    public String getBookList(Model model){
+    public String getBookList(Model model) {
         List<Book> books = bookRepository.findAll();
-        model.addAttribute("books",books);
+        model.addAttribute("books", books);
         return "book_list";
     }
+
     @GetMapping("/new_book")
-    public String newBookForm(Model model){
+    public String newBookForm(Model model) {
         model.addAttribute("book", new Book());
         return "new_book";
     }
-    @PostMapping ("/new_book")
-    public String submitForm(@ModelAttribute Book book, Model model){
+
+    @PostMapping("/new_book")
+    public String submitForm(@ModelAttribute Book book, Model model) {
         model.addAttribute("new_book", book);
         bookRepository.saveAndFlush(book);
         return "redirect:/books";
+    }
+
+
+    @GetMapping("/book_update/{id}")
+    public String getBookToUpdate(@PathVariable("id") Long id, Model model) {
+        Book book = bookRepository.findById(id).orElse(null);
+        bookRepository.delete(book);
+        model.addAttribute("book", new Book());
+        return "new_book";
     }
 
 
