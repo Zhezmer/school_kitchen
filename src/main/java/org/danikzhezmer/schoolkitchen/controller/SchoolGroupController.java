3294f253477b3aed12e6783b477b50a3ca60e454
Danik Zhezmer <danik_zhezmer@mail.ru>
2023-08-30 20:24:20 +0300

    card and list of groups + crud controller

---
+++ src/main/java/org/danikzhezmer/schoolkitchen/controller/SchoolGroupController.java
@@ -0,0 +1,50 @@
+package org.danikzhezmer.schoolkitchen.controller;
+
+import org.danikzhezmer.schoolkitchen.entity.Product;
+import org.danikzhezmer.schoolkitchen.entity.SchoolGroup;
+import org.danikzhezmer.schoolkitchen.repository.SchoolGroupRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("groups")
+public class SchoolGroupController {
+
+    SchoolGroupRepository schoolGroupRepository;
+
+    public SchoolGroupController(SchoolGroupRepository schoolGroupRepository) {
+        this.schoolGroupRepository = schoolGroupRepository;
+    }
+
+    @GetMapping("/{id}")
+    public String getGroup(@PathVariable("id") Long id, Model model) {
+        SchoolGroup group = schoolGroupRepository.findById(id).orElse(null);
+        model.addAttribute("group", group);
+        return "/group/group_card";
+    }
+
+    @GetMapping
+    public String getBookList(Model model) {
+        List<SchoolGroup> groups = schoolGroupRepository.findAll();
+        model.addAttribute("groups", groups);
+        return "group/group_list";
+    }
+    @GetMapping("/new_group")
+    public String newProductForm(Model model) {
+        model.addAttribute("group", new SchoolGroup());
+        List<String> listOfGroups = Arrays.asList("Group1", "Group2", "Group3");
+        model.addAttribute("listOfGroups", listOfGroups);
+        return "group/new_group";
+    }
+
+    @PostMapping("/new_group")
+    public String submitForm(@ModelAttribute SchoolGroup group, Model model) {
+        model.addAttribute("group", group);
+        schoolGroupRepository.save(group);
+        return "redirect:/orders/new_order";
+    }
+}
